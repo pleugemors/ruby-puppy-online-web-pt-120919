fbb83b485a655f11473c16182a5c05a604084a3a
Paul Leugemors <pleugemors@yahoo.com>
2019-12-11 10:08:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,23 @@
 # Add your code here
+class Dog 
+  def initialize(name)
+    @name = name
+    save 
+  end
+  
+  attr_accessor :name 
+  @@all = []
+  
+  def Dog.all
+    @@all
+  end
+  
+  def Dog.print_all
+    @@all.each do |dog|
+      puts dog.name 
+    end
+  end
+  def save
+    @@all << self
+  end
+end
